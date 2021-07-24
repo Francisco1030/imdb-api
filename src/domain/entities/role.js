9c6d5bc446fdfdcdf983ae430995643d4b83e1e3
Francisco Viana <franciscoviana862@gmail.com>
2021-07-24 00:46:31 -0300

    feat: add entity role

---
+++ src/domain/entities/role.js
@@ -0,0 +1,9 @@
+module.exports = class Role {
+  constructor(data) {
+    this.id = data.id;
+    this.name = data.name;
+    this.createdAt = data.createdAt;
+    this.updatedAt = data.updatedAt;
+    this.deletedAt = data.deletedAt;
+  }
+};
