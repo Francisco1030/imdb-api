dfc54f9e0585cfa654b79a52c6b5cd7f1621f64c
Francisco Viana <franciscoviana862@gmail.com>
2021-07-24 00:47:46 -0300

    feat: add contract base-crud-repossitory

---
+++ src/domain/repositories/base-crud-repository.js
@@ -0,0 +1,40 @@
+module.exports = class BaseCrudRepository {
+  /*
+   * @param {Entity} entity
+   * @returns {Entity} entity
+   */
+  create(data) {
+    throw new Error('Method not implement');
+  }
+
+  /*
+   * @param {Object} object
+   * @returns {Entity} entity
+   */
+  fetchOne(where) {
+    throw new Error('Method not implement');
+  }
+
+  /*
+   * @returns {Array} entities
+   */
+  fetchAll() {
+    throw new Error('Method not implement');
+  }
+
+  /*
+   * @param {Entity} entity
+   * @returns {Entity} entity
+   */
+  update(entity) {
+    throw new Error('Method not implement');
+  }
+
+  /*
+   * @param {Sting} id
+   * @returns void
+   */
+  delete(id) {
+    throw new Error('Method not implement');
+  }
+};
