0fb728e6185ed6186408e5dab5c7e53b2914584a
Francisco Viana <franciscoviana862@gmail.com>
2021-07-25 02:26:56 -0300

    project: add jest and eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    node: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error', { singleQuote: true, parser: 'flow' }],
+  },
+};
