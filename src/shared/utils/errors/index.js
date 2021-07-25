b0d293dc0ed69fa9f07b8024b90f2324262d028a
Francisco Viana <franciscoviana862@gmail.com>
2021-07-25 12:45:40 -0300

    feat: add uitils erros

---
+++ src/shared/utils/errors/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  ServerError: require('./server-error'),
+  UnauthorizedError: require('./unauthorized-error'),
+  NotFoundError: require('./not-found-error'),
+  ValidationError: require('./validation-error'),
+};
+
