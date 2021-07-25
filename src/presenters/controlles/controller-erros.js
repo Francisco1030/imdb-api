e9ab6b208d65feda7e0ce46d2194b996cec9ffec
Francisco Viana <franciscoviana862@gmail.com>
2021-07-25 20:43:14 -0300

    feat: add controller erros

---
+++ src/presenters/controlles/controller-erros.js
@@ -0,0 +1,12 @@
+const { NotFoundError, ValidationError } = require('../../shared/utils/errors');
+const HttpResponse = require('../helpers/http-response');
+
+module.exports = class ControllerErrors {
+  static handle(error) {
+    console.error('ERROR:', error);
+    if (error instanceof NotFoundError) return HttpResponse.notFoundError(error);
+    else if (error instanceof ValidationError) return HttpResponse.badRequest(error);
+
+    return HttpResponse.serverError(error);
+  }
+};
