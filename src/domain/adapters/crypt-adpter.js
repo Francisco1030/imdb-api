66a3a750c3f2eb3ebd7a75f34c8b47b6d0d64f73
Francisco Viana <franciscoviana862@gmail.com>
2021-07-26 18:58:03 -0300

    feat: add contract crypt-adpter

---
+++ src/domain/adapters/crypt-adpter.js
@@ -0,0 +1,9 @@
+module.exports = class CryptAdpter {
+  decryptPassword(password, passwordHash) {
+    throw new Error('Method not implement');
+  }
+
+  async encryptPassword(password) {
+    throw new Error('Method not implement');
+  }
+};
