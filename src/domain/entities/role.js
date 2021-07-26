68f58ec3f7a54528199dcc612a90544910099dab
Francisco Viana <franciscoviana862@gmail.com>
2021-07-26 18:57:39 -0300

    refactor: entities user and role

--- src/domain/entities/role.js
+++ src/domain/entities/role.js
@@ -2,8 +2,5 @@ module.exports = class Role {
   constructor(data) {
     this.id = data.id;
     this.name = data.name;
-    this.createdAt = data.createdAt;
-    this.updatedAt = data.updatedAt;
-    this.deletedAt = data.deletedAt;
   }
 };
--- src/domain/entities/user.js
+++ src/domain/entities/user.js
@@ -2,6 +2,8 @@ module.exports = class User {
   constructor(data) {
     this.id = data.id;
     this.name = data.name;
+    this.email = data.email;
+    this.password = data.password;
     this.roleId = data.roleId;
     this.createdAt = data.createdAt;
     this.updatedAt = data.updatedAt;
