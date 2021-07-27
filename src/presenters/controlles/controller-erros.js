c211dc8f41976a91c4a9f8f9be83ad1a837f7ff4
Francisco Viana <franciscoviana862@gmail.com>
2021-07-27 19:48:10 -0300

    feat: add JsonWebTokenError

--- src/presenters/controlles/controller-erros.js
+++ src/presenters/controlles/controller-erros.js
@@ -1,11 +1,16 @@
-const { NotFoundError, ValidationError, ServerError } = require('../../shared/utils/errors');
+const {
+  NotFoundError,
+  ValidationError,
+  ServerError,
+  UnauthorizedError,
+  JsonWebTokenError } = require('../../shared/utils/errors');
 const HttpResponse = require('../helpers/http-response');
-
 module.exports = class ControllerErrors {
   static handle(error) {
     console.error('ERROR:', error);
     if (error instanceof NotFoundError) return HttpResponse.notFoundError(error);
     else if (error instanceof ValidationError) return HttpResponse.badRequest(error);
+    else if (error instanceof JsonWebTokenError) return HttpResponse.unauthorizedError(new UnauthorizedError('Token invalido'));
 
     return HttpResponse.serverError(new ServerError('Erro interno no servidor'));
   }
--- src/shared/utils/errors/index.js
+++ src/shared/utils/errors/index.js
@@ -3,5 +3,6 @@ module.exports = {
   UnauthorizedError: require('./unauthorized-error'),
   NotFoundError: require('./not-found-error'),
   ValidationError: require('./validation-error'),
+  JsonWebTokenError: require('jsonwebtoken').JsonWebTokenError
 };
 
