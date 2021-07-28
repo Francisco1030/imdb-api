85a923c5af0826c682996bd1dfa07f03cd03b9f7
Francisco Viana <franciscoviana862@gmail.com>
2021-07-28 00:59:08 -0300

    feat: add validate-user-service

---
+++ src/infra/services/validate-user-service.js
@@ -0,0 +1,35 @@
+const { ValidationError } = require('../../shared/utils/errors');
+
+module.exports = class ValidateUserService {
+  constructor({ userRepository } = {}) {
+    this.userRepository = userRepository;
+  }
+
+  async validateAdmin(id) {
+    const persistedUser = await this.fetchUser(id);
+    const roleIdAdmin = '8609e912-c66b-4a21-8a38-d2ee0f881d11';
+
+    this.compareRole(persistedUser, roleIdAdmin, 'Usuario não é admin');
+
+    return persistedUser;
+
+  }
+  async validateCommon(id) {
+    const persistedUser = await this.fetchUser({ id });
+    const roleIdCommon = 'd0fd3a89-486f-4826-ba8b-71e5191867c7';
+
+    this.compareRole(persistedUser, roleIdCommon, 'Usuario é admin');
+
+    return persistedUser;
+  }
+
+  async fetchUser(id) {
+    const user = await this.userRepository.fetchOne(id);
+
+    return user;
+  }
+
+  compareRole(user, roleId, message) {
+    if (user.roleId !== roleId) throw new ValidationError(message);
+  }
+};
